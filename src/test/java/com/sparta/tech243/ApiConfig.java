120ef3e399415238b3443ce26bcdd40364dff516
John Herbert <jherbert@spartaglobal.com>
2024-04-12 14:25:52 +0100

    Added the get pet test cases to valid the responses and make sure they are all correct

--- src/test/java/com/sparta/tech243/ApiConfig.java
+++ src/test/java/com/sparta/tech243/ApiConfig.java
@@ -4,50 +4,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ApiConfig {
     private static final Properties properties = new Properties();
 
 
     static {
 
-        try (InputStream inputStream = ApiConfig.class                .getClassLoader()
-
+        try (InputStream inputStream = ApiConfig.class
+                .getClassLoader()
                 .getResourceAsStream("testconfig.properties")) {
-
             if (inputStream != null) {
-
                 properties.load(inputStream);
-
             } else {
-
-                throw new IOException("Unable to find testconfig.properties");
-
-            }        } catch (IOException e) {
-
+                throw new IOException("Unable to find config.properties");
+            }
+        } catch (IOException e) {
             e.printStackTrace();
-
-        }    }
+        }
+    }
 
     public static String getBaseUri() {
-
         return properties.getProperty("api_url");
-
     }
-
-    public static String getToken() {
-
-        return properties.getProperty("api_token");
-
-    }
-
-
-    public static String getCommonBasePath() {
-
-        return properties.getProperty("common_base_path");
-
-    }
-
-    public static String getGithubPersonalToken() {
-        return properties.getProperty("gitHub_personal_access_token");
-    }
+    public static String getToken() {return properties.getProperty("api_token");}
+    public static String getBasePathStore() { return properties.getProperty("store_base_path");}
+    public static String getBasePathUser() {return properties.getProperty("user_base_path");}
+    public static String getBasePet() {return properties.getProperty("pet_base_path");}
 }
