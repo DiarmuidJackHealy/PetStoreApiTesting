6c74464b032ebb95c14f65b917c33557cafa3a3c
Jack Birchall <jackbirchall55@gmail.com>
2024-04-12 11:24:42 +0100

    Added blank test cases to PetGetTests.java

--- src/test/java/com/sparta/tech243/pets/PetsGetTests.java
+++ src/test/java/com/sparta/tech243/pets/PetsGetTests.java
@@ -1,4 +1,30 @@
 package com.sparta.tech243.pets;
 
+import com.sparta.tech243.ApiConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class PetsGetTests {
+
+    public static final String BASE_URI = ApiConfig.getBaseUri();
+    public static final String BASE_PATH = ApiConfig.getPetBasePath();
+
+    @Test
+    @DisplayName("GET /pet/findByStatus")
+    void getPetByStatus() {
+
+    }
+
+    @Test
+    @DisplayName("GET /pet/findByTags")
+    void getPetByTags() {
+
+    }
+
+    @Test
+    @DisplayName("GET /pet/{petId}")
+    void getPetById() {
+
+    }
+
 }
